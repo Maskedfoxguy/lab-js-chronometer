200820d87d076f3b56efa52ba62a1210d76be5c4
Adam Epinger <76474223+Maskedfoxguy@users.noreply.github.com>
2023-10-22 22:35:02 +0200

    Update chronometer.js

--- src/chronometer.js
+++ src/chronometer.js
@@ -29,19 +29,27 @@ class Chronometer {
   }
 
   computeTwoDigitNumber(value) {
-    let digitToString = value
+    let digitToString = value;
+    return ("0" + digitToString.toString()).slice(-2);
     // ... your code goes here
   }
 
   stop() {
+    clearInterval(this.intervalId)
+
+
     // ... your code goes here
   }
 
   reset() {
+    this.currentTime = 0;
+
     // ... your code goes here
   }
 
   split() {
+    
+
     // ... your code goes here
   }
 }
