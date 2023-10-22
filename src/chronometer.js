b90d1e1de5a59064b16ebf746ed2f4734c827b28
Adam Epinger <76474223+Maskedfoxguy@users.noreply.github.com>
2023-10-22 17:35:01 +0200

    Update chronometer.js

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,21 +1,35 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0;
+    this.intervalId = null;
     // ... your code goes here
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
 
+   this.intervalId = setInterval(() => {
+   this.currentTime = this.currentTime + 1;
+    if(printTimeCallback) {
+      printTimeCallback()
+    }
+   }, 1000)
+   
+  }
+  
   getMinutes() {
+    let minutes =  Math.floor(this.currentTime / 60)
+    return minutes;
     // ... your code goes here
   }
 
   getSeconds() {
+    let seconds = Math.floor(this.currentTime % 60)
+    return seconds;
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+    let digitToString = value
     // ... your code goes here
   }
 
